d73f1355163b4d6b49f9278648aa99ebad56c34c
SE-Yukun <yukun@team.anylinks.jp>
2023-05-03 23:21:08 +0000

    update

--- utils/flags.go
+++ utils/flags.go
@@ -15,10 +15,13 @@ type Flags struct {
 func (flags *Flags) Init() {
 	flags.Namespace = flag.String("namespace", "", "Specify the Namespce of the container to operate.")
 	flags.Address = flag.String("address", ":8080", "Specify IP:Port to host api container")
-	flags.Uniquekey = flag.String("key", "", "Used to idnetify witch type of container.\n\tThisOption must be specified")
+	flags.Uniquekey = flag.String("key", "", "Used to idnetify witch type of container.\nThis Option must be specified")
 	flags.Debug = flag.Bool("debug", false, "Debug mode")
 	flag.Parse()
 	if (*flags.Namespace == "") {
 		log.Fatal("Not specified Namespace")
+	}
+	if (*flags.Uniquekey == "") {
+		log. Fatal("Not specified Key")
 	}
 }
